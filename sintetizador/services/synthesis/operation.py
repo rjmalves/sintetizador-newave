64a236b0560a148ef3f872f9c75f5347ea376a3c
Rogerio <rogerioalves.ee@gmail.com>
2022-09-29 20:34:21 -0300

    fix indice estagio

--- sintetizador/services/synthesis/operation.py
+++ sintetizador/services/synthesis/operation.py
@@ -451,6 +451,8 @@ class OperationSynthetizer:
         starting_date = datetime(
             year=dger.ano_inicio_estudo, month=dger.mes_inicio_estudo, day=1
         )
+        df_starting = df.loc[df["Data Inicio"] >= starting_date]
+        df_starting["Estagio"] -= int(df_starting["Estagio"].min()) - 1
         return df.loc[df["Data Inicio"] >= starting_date]
 
     @classmethod
