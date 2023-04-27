5b75154e31f54d3119099d92f6fdf3f34903f208
Rogerio Alves <rogerioalves.ee@gmail.com>
2023-04-27 16:13:54 -0300

    inclui mais testes

--- tests/sintetizador/adapters/repository/test_files.py
+++ tests/sintetizador/adapters/repository/test_files.py
@@ -77,6 +77,90 @@ def test_get_pmo(test_settings):
     )
 
 
+def test_get_hidr(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    hidr = repo.get_hidr()
+    assert isinstance(hidr.cadastro, pd.DataFrame)
+
+
+def test_get_cortes(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    cortes = repo.get_nwlistcf_cortes()
+    assert isinstance(cortes.cortes, pd.DataFrame)
+
+
+def test_get_estados(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    estados = repo.get_nwlistcf_estados()
+    assert isinstance(estados.estados, pd.DataFrame)
+
+
+def test_get_energiaf(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    energiaf = repo.get_energiaf(1)
+    assert isinstance(energiaf.series, pd.DataFrame)
+
+
+def test_get_energiab(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    energiab = repo.get_energiab()
+    assert isinstance(energiab.series, pd.DataFrame)
+
+
+def test_get_energias(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    energias = repo.get_energias()
+    assert isinstance(energias.series, pd.DataFrame)
+
+
+def test_get_enavazf(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    enavazf = repo.get_enavazf(1)
+    assert isinstance(enavazf.series, pd.DataFrame)
+
+
+def test_get_enavazb(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    enavazb = repo.get_enavazb()
+    assert isinstance(enavazb.series, pd.DataFrame)
+
+
+def test_get_enavazs(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    enavazs = repo.get_enavazs()
+    assert isinstance(enavazs.series, pd.DataFrame)
+
+
+def test_get_vazaof(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    vazaof = repo.get_vazaof(1)
+    assert isinstance(vazaof.series, pd.DataFrame)
+
+
+def test_get_vazaob(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    vazaob = repo.get_vazaob()
+    assert isinstance(vazaob.series, pd.DataFrame)
+
+
+def test_get_vazaos(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    vazaos = repo.get_vazaos()
+    assert isinstance(vazaos.series, pd.DataFrame)
+
+
+def test_get_vazoes(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    vazoes = repo.get_vazoes()
+    assert isinstance(vazoes.vazoes, pd.DataFrame)
+
+
+def test_get_engnat(test_settings):
+    repo = factory("FS", DECK_TEST_DIR)
+    engnat = repo.get_engnat()
+    assert isinstance(engnat.series, pd.DataFrame)
+
+
 def test_get_nwlistop(test_settings):
     repo = factory("FS", DECK_TEST_DIR)
     assert isinstance(
